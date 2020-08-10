3da86be3686f249bc62c17f93fa32d79221bad4d
davidchomczyk78 <68852078+davidchomczyk78@users.noreply.github.com>
2020-08-10 10:56:20 -0300

    Add files via upload
    nuevo

--- dos.js
+++ dos.js
@@ -7,5 +7,77 @@ b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
 */function mostrar()
 {
-  alert("dos");
+
+ var marca;
+ var cantidad;
+ var precio;
+ var pesoKilogramo;
+ var tipo;
+ var respuesta = "si";
+ var Primeramarca = true;
+ var MarcaMascaraLiquido;
+ var tipoLiquidoMasCaro;
+ var precioMasCaro;
+ var tipoSolidoMasLiviano;
+ var MarcaMasLiviano;
+ var PrimeraMarcaLiviana = true;
+ var acumuladorPesototal = 0;
+ var contadorpeso =0;
+
+    respuesta == "si";
+    while(respuesta =="si"){
+
+      marca = prompt("ingrese marca del poducto");
+      cantidad = parseInt(prompt("ingrese cantidad de producto"));
+
+      precio = parseFloat(prompt("ingrese precio"));
+        while(isNaN(precio)&& precio >1)
+        {
+      precio = parseInt(prompt(" Error ingrese un importe"));    
+        }
+      pesoKilogramo = parseFloat(prompt("ingrese el peso en kilogramos "));
+        while(isNaN(pesoKilogramo)&& pesoKilogramo >1)
+        {
+      pesoKilogramo = parseFloat(prompt(" Error ingrese el peso en kilogramos "));
+        }
+      tipo = prompt("ingrese el tipo de producto sea 'liquido' o 'solido':");
+        while(tipo !="solido" && tipo!="liquido"){
+      tipo = prompt(" ERROR ingrese el tipo de producto sea 'liquido' o 'solido':");
+        } 
+        // b -la marca del más caro de los líquidos
+        if(Primeramarca == true){
+          MarcaMascaraLiquido = marca;
+          tipoLiquidoMasCaro = tipo;
+          precioMasCaro = precio;
+          Primeramarca = false;
+        }
+        else
+        {
+          if(MarcaMascaraLiquido > tipoLiquidoMasCaro)
+          {
+          MarcaMascaraLiquido = marca;
+          tipoLiquidoMasCaro = tipo;
+          }
+          // la marca del mas liviano
+        }
+        if(PrimeraMarcaLiviana == true && tipoSolidoMasLiviano > MarcaMasLiviano){
+          tipoSolidoMasLiviano = tipo;
+          MarcaMasLiviano = marca;
+          PrimeraMarcaLiviana = false;
+        }
+// informar el peso total de la compra.
+acumuladorPesototal+=cantidad;
+contadorpeso++;
+
+
+
+
+      respuesta = prompt(" quiere seguir");
+    }
+    
+
+    document.write("elpeso total de la compra es de: " + acumuladorPesototal + " <br>");
+    document.write(" la marca mas cara es" + MarcaMascaraLiquido + " <br>" );
+    document.write(" la marca del mas liviano es:" + MarcaMasLiviano + "<br>");
+
 }
--- uno.js
+++ uno.js
@@ -9,5 +9,84 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	var nombre;
+	var temperatura;
+	var sexo;
+	var edad;
+	var acumuladorEdad =0;
+	var contadorEdad =0;
+	var contadorSexoFemenino= 0;
+	var contadorSexoMasculino =0;
+	var acumuladorFemenino =0;
+	var acumuladorMasculino =0;
+	var PromedioTotalEdad;
+	var SexoFemeninoMasTemperatura;
+	var MasTemperatura;
+	var primeraTempratura = true;
+	var mensajeTemperatura = " nose ingresaron mujeres";
+
+	 for(var i = 0; i<2; i++){
+
+		nombe = prompt(" ingrese nombre");
+			while(nombre)
+			{
+		nombre = prompt(" error ingrese las opcion  pedida");
+			}
+		temperatura = parseFloat(prompt(" ingrese temperatura"));
+			while(isNaN(temperatura)&& temperatura <1){
+		temperatura = parseFloat(prompt("Error ingrese bien la temperatura"));		
+			}	
+		sexo = prompt(" ingrese sexo 'f' o 'm'");
+			while(sexo!="f" && sexo!="m"){
+		sexo = prompt(" Error ingrese sexo 'f' o 'm'");	
+			}
+		edad = parseInt(prompt("ingrese edad "));
+			while(isNaN(edad)&& edad <1){
+		edad = prompt(" Error ingrese correctamente lo seleccionado");		
+			}
+			acumuladorEdad+=edad;
+			contadorEdad++;
+
+
+			if(primeraTempratura ==true){
+				SexoFemeninoMasTemperatura = sexo;
+				MasTemperatura = temperatura;
+				primeraTempratura = false;
+
+			}else
+			{if(SexoFemeninoMasTemperatura > MasTemperatura)
+				{
+					SexoFemeninoMasTemperatura = sexo;
+				MasTemperatura = temperatura;
+			}
+
+			}
+
+			switch(sexo){
+				case "m":
+					acumuladorMasculino+=sexo;
+					contadorSexoMasculino++;
+
+					break;
+				case "f":
+					acumuladorFemenino+=sexo;
+					contadorSexoFemenino++;
+					break;	
+			}
+			if(primeraTempratura == 1){
+			 mensajeTemperatura = " no  se ingresaron mujeres ";
+			 }
+			
+	 }
+	
+
+
+PromedioTotalEdad = acumuladorEdad / contadorEdad;
+
+	document.write("cantidad de personas de  cada sexo  femenino es: " + contadorSexoFemenino + " y el masculino es :" + contadorSexoMasculino +">br>");
+	document.write(" la edad promedio tatol es : " + PromedioTotalEdad + "<br>");
+	document.write("el sexo femenino con mas temperatura es :" + MasTemperatura + "<br>");
+	document.write(mensajeTemperatura);
+	
+
 }
